5593ff1947faad188c2d47f7146deb2e7811ccc6
natlyrrad <58030868+natlyrrad@users.noreply.github.com>
2019-11-26 19:21:41 +0000

    Update MainView.java

--- My-Starter-Project/src/main/java/com/packagename/myapp/MainView.java
+++ My-Starter-Project/src/main/java/com/packagename/myapp/MainView.java
@@ -1,6 +1,10 @@
 package com.packagename.myapp;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.login.AbstractLogin;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -14,8 +18,26 @@ import com.vaadin.flow.server.PWA;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me",
+        Button button = new Button("Clickme",
                 event -> Notification.show("Clicked!"));
         add(button);
+
+        LoginOverlay component = new LoginOverlay();
+        component.addLoginListener(e -> component.close());
+        Button open = new Button("Open login overlay",
+                e -> component.setOpened(true));
+
+        LoginI18n i18n = LoginI18n.createDefault();
+        i18n.setAdditionalInformation("To close the login form submit non-empty username and password");
+        component.setI18n(i18n);
+        add(open);
+        add(component);
+    }
+
+    private boolean authenticate(AbstractLogin.LoginEvent e) {
+        return true;
+    }
+
+    private void navigateToMainPage() {
     }
 }
